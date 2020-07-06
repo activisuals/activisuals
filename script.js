be2a9b45211d463e22a824dfa010641ecfa554d9
activisuals <67645624+activisuals@users.noreply.github.com>
2020-07-06 17:22:30 +0100

    activisuals website

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000);
-}
+// fetch('https://api.unsplash.com/photos/random/?count=5&client_id=52d8369eb3e2576a5f5b6423865e074e9c7045761bff1ac5664ff3e0bdb57a1d') 
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEaach(function(image, i) {
+//       document.querySelector("#slide-" + (i+1)).innerHTML = `
+//         <img src="${image.urls.regular}" alt="">
+//         <p class="author-info">
+//           <a href="${image.links.html}?utm_source=slider-thing&utm_medium=referral&utm_campaign=api-credit">Photo by ${image.user.name}</a> on <a href="https://unsplash.com/">Unsplash</a>
+//         </p>
+//       `;
+//     });
+//   });
